23f53210565b5785a05856f05a105de8be9769ec
Aya Elsayed <aelsayed12@bloomberg.net>
2022-09-01 09:02:51 +0100

    new workflow

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -23,7 +23,7 @@ author = 'Aya Elsayed'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.rst
+++ docs/index.rst
@@ -11,6 +11,7 @@ Welcome to The Office's documentation!
    :caption: Contents:
 
 
+   modules
 
 Indices and tables
 ==================
